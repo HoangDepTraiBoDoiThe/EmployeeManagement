ea002fee9ff14e64dd0ed43571bf5ae86ff56715
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-07-29 15:48:31 +0700

    Add user model

---
+++ models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.models;
+
+public class User
+{
+    [Key]
+    private int Id { get; set; }
+    
+    [StringLength(100, MinimumLength = 2)]
+    [Required(ErrorMessage = "User name is required.")]
+    private string UserName { get; set; }
+    
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 12, ErrorMessage = "Password has to be at least 12 characters")]
+    [Required(ErrorMessage = "Password is required.")]
+    private string Password { get; set; }
+    
+}
