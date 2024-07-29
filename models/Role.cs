e95828596f2e608965d6b55db3db6dcaaa697975
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-07-29 15:52:24 +0700

    Add role model

---
+++ models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.models;
+
+public class Role
+{
+    [Key]
+    private int Id { get; set; }
+    
+    [Required]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [StringLength(30)]
+    private string RoleName { get; set; }
+}
