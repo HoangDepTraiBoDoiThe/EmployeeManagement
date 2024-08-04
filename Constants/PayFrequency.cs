d6f6dc1383e2799810b08011fbc9b8e9131de64d
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-04 22:24:10 +0700

    Add employee wage model

---
+++ Constants/PayFrequency.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagement.Constants;
+
+public enum PayFrequency
+{
+    PAYFREQUENCY_WEEKLY,
+    PAYFREQUENCY_MONTHLY,
+    PAYFREQUENCY_DAILY
+}
---
+++ models/EmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using EmployeeManagement.Constants;
+
+namespace EmployeeManagement.models;
+
+public class EmployeeWage
+{
+    [Key]
+    public int WageId { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal BaseSalary { get; set; }
+    [Required]
+    public PayFrequency PayFrequency { get; set; }
+
+    [Required]
+    public int EmployeeId { get; set; }
+    [ForeignKey(nameof(EmployeeId))]
+    public Employee Employee { get; set; } = null!;
+
+    public EmployeeWage()
+    {
+    }
+    
+    public EmployeeWage(int wageId, decimal baseSalary, PayFrequency payFrequency, int employeeId, Employee employee)
+    {
+        WageId = wageId;
+        BaseSalary = baseSalary;
+        PayFrequency = payFrequency;
+        EmployeeId = employeeId;
+        Employee = employee;
+    }
+}
