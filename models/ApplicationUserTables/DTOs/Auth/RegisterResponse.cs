fb0dbe2962a8318b5d9179f9d7da54ab2c6e047f
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-08-15 21:26:07 +0700

    Delete jwt remain peaces

--- models/ApplicationUserTables/DTOs/Auth/RegisterResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿using EmployeeManagement.Helper;
-
-namespace EmployeeManagement.Models.ApplicationUserTables.DTOs.Auth;
-
-public class RegisterResponse(string responseMessage) : GeneralResponse(responseMessage)
-{
-}
